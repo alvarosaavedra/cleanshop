64b2f6bd8ed223b2cd1eb8fb92269b5f5325f883
AlvaroSaavedra <alvaro@chipax.com>
2020-08-06 20:12:15 -0400

    refactor: Mejora la configuracion del test del OrderUseCase

--- tests/UseCases/OrderUseCases.test.ts
+++ tests/UseCases/OrderUseCases.test.ts
@@ -4,19 +4,32 @@ import {OrderUseCases} from "../../src/UseCases/OrderUseCases";
 import {OrderRepositoryInterface} from "../../src/interfaces/OrderRepositoryInterface";
 import {ProductRepositoryInterface} from "../../src/interfaces";
 
+function config() {
+    const mockOrderRepo = mock<OrderRepositoryInterface>();
+    const mockProductRepo = mock<ProductRepositoryInterface>();
+    return {mockOrderRepo, mockProductRepo};
+}
+
 describe("OrderUseCase", ()=> {
+    describe("getById", ()=>{
+        it("Should call OrderRepo and return the value", async ()=> {
+            const {mockOrderRepo, mockProductRepo} = config();
+            const mockProcessorsFactory = jest.fn((paymentProvider) => paymentProvider)
+
+        })
+    })
+
     describe("pay",() => {
         it("should throw a error if the order don't exists", async ()=> {
-            const mockOrderRepo = mock<OrderRepositoryInterface>();
-            const mockProductRepo = mock<ProductRepositoryInterface>();
+            const {mockOrderRepo, mockProductRepo} = config();
+            const mockProcessorsFactory = jest.fn((paymentProvider) => paymentProvider)
             mockOrderRepo.getById.mockRejectedValue(Error("Order no existe"));
-            const useCase = new OrderUseCases(mockOrderRepo, mockProductRepo, (method: any) => method)
+            const useCase = new OrderUseCases(mockOrderRepo, mockProductRepo, mockProcessorsFactory)
             await expect(useCase.pay(4, "trsnback")).rejects
                 .toThrowError("Order no existe")
         })
         it("should throw error if payment method is not implemented", async ()=>{
-            const mockOrderRepo = mock<OrderRepositoryInterface>();
-            const mockProductRepo = mock<ProductRepositoryInterface>();
+            const {mockOrderRepo, mockProductRepo} = config();
             const mockProcessorsFactory = jest.fn((paymentProvider) => {
                 throw Error("Metodo de pago invalido")
             })
